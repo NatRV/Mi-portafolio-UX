7b6f3e07786f9050875e77c982ecfe640147e9ba
NatRV <n.riquelme.veliz@gmail.com>
2019-12-23 14:55:41 -0300

    1era edición plantilla,prueba

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Nataly Riquelme.", "Diseñadora UX.", "Diseñadora UI.",  "Diseñadora Industrial"],
       typeSpeed: 100,
       loop: true,
     });
