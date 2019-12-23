2f80a2d549673c39f5ef820219610a8a87e443e2
NatRV <n.riquelme.veliz@gmail.com>
2019-12-23 15:02:43 -0300

    probando 2da edición

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Nataly Riquelme.", "Diseñadora UX.", "Diseñadora UI.",  "Diseñadora Industrial"],
+      strings: ["Nataly Riquelme.", "Diseñadora UX.", "Diseñadora UI.", "Diseñadora Industrial"],
       typeSpeed: 100,
       loop: true,
     });
